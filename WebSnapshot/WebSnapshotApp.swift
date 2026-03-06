0156a5412bfaf420e48d0a6dbe4f0af44e2f2010
flucium <flucium@flucium.net>
2026-03-07 08:41:18 +0900

    update ./WebSnapshot/WebSnapshotApp.swift
    support: dark mode icon.

--- WebSnapshot/WebSnapshotApp.swift
+++ WebSnapshot/WebSnapshotApp.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import AppKit
+
 
 @main
 struct WebSnapshotApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             HomeView()
+        }
+    }
+}
+
+func updateAppIcon() {
+    let appearance = NSApplication.shared.effectiveAppearance
+    let isDark = appearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
+
+    let iconName = isDark ? "IconDark" : "IconLight"
+
+    if let image = NSImage(named: iconName) {
+        NSApplication.shared.applicationIconImage = image
+    }
+}
+
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        updateAppIcon()
+
+        DistributedNotificationCenter.default().addObserver(
+            forName: NSNotification.Name("AppleInterfaceThemeChangedNotification"),
+            object: nil,
+            queue: .main
+        ) { _ in
+            updateAppIcon()
         }
     }
 }
