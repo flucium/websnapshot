1c41898650bc549d71681b9610e42167ed22ce21
flucium <flucium@flucium.net>
2026-03-06 13:39:56 +0900

    refactor ./WebSnapshot/Views/Components/WebView.swift

--- WebSnapshot/Views/Components/WebView.swift
+++ WebSnapshot/Views/Components/WebView.swift
@@ -15,18 +15,6 @@ struct WebViewContainer: NSViewRepresentable {
     }
 
     func updateNSView(_ nsView: WKWebView, context: Context) {
-    }
-}
-
-
-
-struct MultipleWebView: NSViewRepresentable {
-    let webView: WKWebView
-
-    func makeNSView(context: Context) -> WKWebView {
-        webView
-    }
-
-    func updateNSView(_ nsView: WKWebView, context: Context) {
+        
     }
 }
