0e17972c137d0fec24c419b28ea6d0bb4f5045d7
flucium <flucium@flucium.net>
2026-03-06 11:49:30 +0900

    add ./WebSnapshot/WebSnapshotApp.swift

---
+++ WebSnapshot/WebSnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSnapshotApp.swift
+//  WebSnapshot
+//
+//  Created by flucium on 2026/03/06.
+//
+
+import SwiftUI
+
+@main
+struct WebSnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
