987928eb096b4f92de722be1db93aa1313f6dc7e
flucium <flucium@flucium.net>
2026-03-07 15:22:33 +0900

    update ./WebSnapshot/Models/PDFHistoryEntry.swift

---
+++ WebSnapshot/Models/PDFHistoryEntry.swift
@@ -0,0 +1,28 @@
+//
+//  PDFHistoryEntry.swift
+//  WebSnapshot
+//
+//  Created by flucium on 2026/03/07.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class PDFHistoryEntry {
+    var path: String
+
+    init(path: String) {
+        self.path = path
+    }
+}
+
+extension PDFHistoryEntry {
+    var fileURL: URL {
+        URL(fileURLWithPath: path)
+    }
+
+    var fileName: String {
+        fileURL.lastPathComponent
+    }
+}
