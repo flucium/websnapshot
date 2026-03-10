bbdf18a52bcfbda93f0ee9efa4a33169989c9456
flucium <flucium@flucium.net>
2026-03-11 01:06:37 +0900

    git update ./WebSnapshot/Features/HomeView.swift
    added: NavigationDestination, @ViewBuilder, NavigationSplitView.

--- WebSnapshot/Features/HomeView.swift
+++ WebSnapshot/Features/HomeView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 
+private enum NavigationDestination: Hashable {
+    case single
+    case multiple
+    case history
+}
+
 struct HomeView: View {
+    
+    @State private var destination: NavigationDestination? = .single
+    
+    @ViewBuilder
+    private func detail(
+        for destination: NavigationDestination?
+    ) -> some View {
+        switch destination {
+        case .single:
+            EmptyView()
+        case .multiple:
+            EmptyView()
+        case .history:
+            EmptyView()
+        default:
+            EmptyView()
+        }
+    }
+
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationSplitView {
+            List(
+                selection: $destination
+            ) {
+
+                NavigationLink(
+                    value: NavigationDestination.single
+                ) {
+                    Label(
+                        "Single",
+                        systemImage: "magnifyingglass"
+                    )
+                }
+                NavigationLink(
+                    value: NavigationDestination.multiple
+                ) {
+                    Label(
+                        "Multiple",
+                        systemImage: "magnifyingglass.circle"
+                    )
+                }
+                NavigationLink(
+                    value: NavigationDestination.history
+                ) {
+                    Label(
+                        "History",
+                        systemImage: "list.bullet.circle"
+                    )
+                }
+            }
+        } detail: {
+            detail(
+                for: destination
+            )
+
+        }
+
     }
 }
 
