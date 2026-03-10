25cb7feda0d78f83789c5bd5de0145723b725e26
flucium <flucium@flucium.net>
2026-03-11 00:52:10 +0900

    git add ./Features/HomeView.swift
    View entry point.

---
+++ WebSnapshot/Features/HomeView.swift
@@ -0,0 +1,13 @@
+//
+//  HomeView.swift
+//  WebSnapshot
+//
+//  Created by flucium on 2026/03/11.
+//
+
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
