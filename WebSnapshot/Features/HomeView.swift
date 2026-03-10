7568f63c352c2ec0397a31c6497f527c390e743f
flucium <flucium@flucium.net>
2026-03-11 01:02:18 +0900

    git add ./WebSnapshot/Features/HomeView.swift

--- WebSnapshot/Features/HomeView.swift
+++ WebSnapshot/Features/HomeView.swift
@@ -5,9 +5,15 @@
 //  Created by flucium on 2026/03/11.
 //
 
+import SwiftUI
+
 
 struct HomeView: View {
     var body: some View {
         Text("Hello, World!")
     }
+}
+
+#Preview {
+    HomeView()
 }
