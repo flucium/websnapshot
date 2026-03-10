145e30bc02f6c3769d9c18dd108f6b766a98d317
flucium <flucium@flucium.net>
2026-03-10 22:56:33 +0900

    add WebSnapshot/Views/Components/WebView.swift

---
+++ WebSnapshot/Views/Components/WebView.swift
@@ -0,0 +1,21 @@
+//
+//  WebView.swift
+//  WebSnapshot
+//
+//  Created by flucium on 2026/03/10.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebViewContainer: NSViewRepresentable {
+    let webView: WKWebView
+
+    func makeNSView(context: Context) -> WKWebView {
+        webView
+    }
+
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        
+    }
+}
