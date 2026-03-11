c21ef3c92b077b17fff8ba84d6d63025ee024b53
flucium <flucium@flucium.net>
2026-03-11 10:44:59 +0900

    Create WebState.swift

---
+++ WebSnapshot/Shared/States/WebState.swift
@@ -0,0 +1,112 @@
+//
+//  WebViewStatel.swift
+//  WebSnapshot
+//
+//  Created by flucium on 2026/03/11.
+//
+
+import Combine
+import WebKit
+
+@MainActor
+class WebState: ObservableObject {
+    @Published var urlString: String = ""
+    @Published var status: String = ""
+    @Published var appError: AppError? = nil
+    @Published var exportData: Data? = nil
+    @Published var pdfFileDocument:PDFFileDocument?
+    @Published var isExporting: Bool = false
+    private var isClearingWebView = false
+    private var canExportPDF: Bool = true
+    
+    
+    let navigationDelegate = NavigationDelegate()
+
+    let wkWebView = WKWebView(frame: .zero)
+    
+    init(){
+        wkWebView.navigationDelegate = navigationDelegate
+       
+        navigationDelegate.onFinish = { [weak self] in
+            guard let self else { return }
+            
+            if self.isClearingWebView {
+                self.isClearingWebView = false
+                self.canExportPDF = false
+                return
+            }
+            self.clearError()
+            self.status = ""
+        }
+       
+        navigationDelegate.onError = { [weak self] message in
+            self?.canExportPDF = false
+            self?.setError(.display(message: "Load failed: \(message)"))
+        }
+        
+    }
+    
+    func load(){
+        guard let url = URL.normalized(from: urlString) else {
+            setError(.invalidURL)
+            return
+        }
+
+        canExportPDF = false
+        clearError()
+        status = "Loading..."
+        wkWebView.load(URLRequest(url: url))
+    }
+    
+    func clear() {
+        clearError()
+        wkWebView.loadHTMLString("", baseURL: nil)
+        exportData = nil
+         pdfFileDocument = nil
+        urlString = ""
+        status = ""
+        isClearingWebView = true
+        isExporting = false
+    }
+    
+    
+    func setError(_ appError: AppError) {
+        self.appError = appError
+        status = appError.errorDescription ?? "unknown"
+    }
+
+    func setError(_ error: Error) {
+        setError(AppError(error: error))
+    }
+  
+    func clearError() {
+        appError = nil
+    }
+    
+    func setCanExport(caExport:Bool){
+        self.canExportPDF = caExport
+    }
+    
+    func makePDF(webView: WKWebView) async throws -> Data {
+        try await pdfData(from: webView)
+    }
+
+}
+
+
+private func pdfData(from webView: WKWebView) async throws -> Data {
+    try await withCheckedThrowingContinuation { continuation in
+        let config = WKPDFConfiguration()
+
+        webView.createPDF(configuration: config) { result in
+            switch result {
+            case .success(let data):
+                continuation.resume(returning: data)
+
+            case .failure(let error):
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+}
+
