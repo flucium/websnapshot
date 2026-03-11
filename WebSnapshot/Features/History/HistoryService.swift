dc5cb0cfa77f2a163dca54ff73aeb395015b1aee
flucium <flucium@flucium.net>
2026-03-11 11:09:39 +0900

    Create HistoryService.swift

---
+++ WebSnapshot/Features/History/HistoryService.swift
@@ -0,0 +1,95 @@
+//
+//  HistoryService.swift
+//  WebSnapshot
+//
+//  Created by flucium on 2026/03/11.
+//
+
+
+import Foundation
+import CoreGraphics
+
+func deletePDFFile(url:URL) throws {
+    
+    switch FileIO().exists(url: url) {
+    case .success(true):
+        break
+    case .success(false):
+        throw CocoaError(.fileNoSuchFile)
+    case .failure:
+        
+        throw CocoaError(.fileNoSuchFile)
+    }
+    
+    guard isPDFFile(url: url) else {
+        throw CocoaError(.fileReadCorruptFile)
+    }
+    
+    
+    switch FileIO().delete(url: url) {
+    case .success:
+        break
+    case .failure:
+        throw CocoaError(.fileWriteNoPermission)
+    }
+    
+}
+
+
+func isPDFFile(url: URL) -> Bool {
+  
+    do {
+        
+        guard let fileSize = ((try FileManager.default.attributesOfItem(atPath: url.path))[.size] as? NSNumber)?.uint64Value, fileSize >= 8 else {
+            return false
+        }
+        
+        let fileHandle = try FileHandle(forReadingFrom: url)
+        
+        defer {
+            try? fileHandle.close()
+        }
+        
+        
+        let fileHeader = try fileHandle.read(upToCount: 8) ?? Data()
+        guard fileHeader.count >= 8 else {
+            return false
+        }
+        
+        guard fileHeader.starts(with: Data("%PDF-".utf8)) else {
+            return false
+        }
+        
+        
+        guard let version = String(data: fileHeader.subdata(in: 5..<8), encoding: .ascii),
+              version.range(of: #"^1\.[0-7]$"#, options: .regularExpression) != nil else {
+            
+            return false
+        }
+        
+        
+        try fileHandle.seek(toOffset: fileSize - UInt64(Int(min(fileSize, 4096))))
+        
+        
+        let end = try fileHandle.readToEnd() ?? Data()
+        
+        guard end.range(of: Data("startxref".utf8), options: .backwards) != nil else {
+            return false
+            
+        }
+    
+        guard end.range(of: Data("%%EOF".utf8), options: .backwards) != nil else {
+            return false
+        }
+        
+    } catch {
+        return false
+    }
+
+    guard let doc = CGPDFDocument(url as CFURL) else {
+        return false
+    }
+    
+    return doc.numberOfPages > 0
+    
+}
