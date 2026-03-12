b3a04cc3daabc973d441a09142ecf3e07df5d0c1
flucium <flucium@flucium.net>
2026-03-12 13:15:33 +0900

    Update HistoryService.swift

--- WebSnapshot/Features/History/HistoryService.swift
+++ WebSnapshot/Features/History/HistoryService.swift
@@ -10,7 +10,7 @@ import Foundation
 import PDFKit
 
 func deletePDFFile(url: URL) throws {
-    guard FileManager.default.fileExists(atPath: url.path) else {
+    guard withSecurityScopedAccess(url: url, action: { FileManager.default.fileExists(atPath: url.path) }) else {
         throw CocoaError(.fileNoSuchFile)
     }
 
@@ -19,20 +19,34 @@ func deletePDFFile(url: URL) throws {
     }
 
     do {
-        try FileManager.default.removeItem(at: url)
+        try withSecurityScopedAccess(url: url) {
+            try FileManager.default.removeItem(at: url)
+        }
     } catch {
         throw CocoaError(.fileWriteNoPermission)
     }
 }
 
 func isPDFFile(url: URL) -> Bool {
-    guard FileManager.default.fileExists(atPath: url.path) else {
+    guard withSecurityScopedAccess(url: url, action: { FileManager.default.fileExists(atPath: url.path) }) else {
         return false
     }
 
-    guard let document = PDFDocument(url: url) else {
+    guard let document = withSecurityScopedAccess(url: url, action: { PDFDocument(url: url) }) else {
         return false
     }
 
     return document.pageCount > 0
+}
+
+private func withSecurityScopedAccess<T>(url: URL, action: () throws -> T) rethrows -> T {
+#if os(macOS)
+    let didAccess = url.startAccessingSecurityScopedResource()
+    defer {
+        if didAccess {
+            url.stopAccessingSecurityScopedResource()
+        }
+    }
+#endif
+    return try action()
 }
