5265564e991d7d4702b4fe2179fe44d2018c8c87
flucium <flucium@flucium.net>
2026-04-23 07:24:00 +0900

    add ./WebSnapshot/WebSnapshot/WebSnapshotApp.swift
    added WebSnapshotApp.swift. program entry point.

---
+++ WebSnapshot/WebSnapshot/WebSnapshotApp.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct WebSnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+        .modelContainer(for: [
+            Directory.self,
+            DirectoryHistory.self,
+        ])
+    }
+}
