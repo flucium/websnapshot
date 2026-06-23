5194e26f004bf6ccccfab9e3b4ba33e3417e8d95
flucium <flucium@flucium.net>
2026-06-24 08:39:52 +0900

    update ./WebSnapshot/Shared/Views/FilePanel.swift. Discontinued support for iOS. support: macOS only.

---
+++ WebSnapshot/Shared/Views/FilePanel.swift
@@ -0,0 +1,37 @@
+import Foundation
+import AppKit
+import UniformTypeIdentifiers
+
+@MainActor
+func savePanel(
+    _ title: String,
+    _ url: URL?,
+    _ pdfFileDocument: PDFFileDocument?
+) throws -> URL? {
+    guard let data = pdfFileDocument?.data else {
+        return nil
+    }
+
+    let panel = NSSavePanel()
+
+    panel.allowedContentTypes = [.pdf]
+    panel.canCreateDirectories = true
+    panel.title = "Save"
+    panel.nameFieldStringValue = URL.pdfFileName(title, url)
+
+    FileManager.default.urls(
+        for: .documentDirectory,
+        in: .userDomainMask
+    ).first.map { panel.directoryURL = $0 }
+
+    guard panel.runModal() == .OK, let destination = panel.url else {
+        return nil
+    }
+
+    do {
+        try data.write(to: destination, options: .atomic)
+        return destination
+    } catch {
+        throw AppError.invalidIO("Write data to \(destination.path) failed.")
+    }
+}
