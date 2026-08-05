ad2c329ae13b947202d8a36a322f944a221704c2
flucium <flucium@flucium.net>
2026-08-05 21:22:42 +0900

    WebSnapshot/WebSnapshotApp.swift
    set minimum window size to prevent layout issues. change: WindowGroup{...}...

--- WebSnapshot/WebSnapshotApp.swift
+++ WebSnapshot/WebSnapshotApp.swift
@@ -6,7 +6,11 @@ struct WebSnapshotApp: App {
     var body: some Scene {
         WindowGroup {
             HomeView()
-        }.modelContainer(for: [
+                .frame(minWidth: 730, minHeight: 400)
+        }
+        .defaultSize(width: 1000, height: 600)
+        .windowResizability(.contentMinSize)
+        .modelContainer(for: [
             AppearanceSettings.self,
             PDFFile.self,
             PDFTag.self,
