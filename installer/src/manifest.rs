596f1d43696537d306a0d454889ce61e5db41d0d
flucium <flucium@flucium.net>
2026-08-06 00:10:22 +0900

    add installer/src/manifest.rs

---
+++ installer/src/manifest.rs
@@ -0,0 +1,14 @@
+/// WebSnapshot installer name
+pub const APP_INSTALLER_NAME:&str = "WebSnapshotInstaller";
+
+/// WebSnapshot installer version
+pub const APP_INSTALLER_VERSION:&str = "0.0.1";
+
+// WebSnapshot global(Macintosh HD/Applications) install path
+// pub const APP_GLOBAL_INSTALL_PATH:&str = "/Applications/";
+
+/// WebSnapshot local($HOME/Applications/) install path
+pub const APP_LOCAL_INSTALL_PATH:&str = "$HOME/Applications/";
+
+/// WebSnapshot local($HOME/Applications/WebSnapshot) install path
+pub const APP_LOCAL_PATH:&str = "$HOME/Applications/WebSnapshot.app";
