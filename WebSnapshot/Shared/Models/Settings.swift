8516a2e8f5738b2571ef3a295405f14f7243593a
flucium <flucium@flucium.net>
2026-08-20 14:14:15 +0900

    update: WebSnapshot/Shared/Models/Settings.swift
    removed service: Storage Service.

--- WebSnapshot/Shared/Models/Settings.swift
+++ WebSnapshot/Shared/Models/Settings.swift
@@ -21,38 +21,3 @@ extension AppearanceSettings{
         }
     }
 }
-
-@Model class StorageSettings{
-    var storage: Storage
-    var fixedStoragePath: String
-    
-    init(_ storage: Storage) {
-        self.storage = storage
-        
-        self.fixedStoragePath = "$HOME/Documents/WebSnapshot/"
-    }
-    
-    init(_ storage: Storage,_ fixedStoragePath: String) {
-        
-        self.storage = storage
-        
-        if storage == .fixed && !fixedStoragePath.isEmpty{
-            self.fixedStoragePath = fixedStoragePath
-        }else{
-            self.fixedStoragePath = "$HOME/Documents/WebSnapshot/"
-        }
-    }
-}
-
-extension StorageSettings{
-    enum Storage: String, CaseIterable, Codable, Hashable, Identifiable {
-        case fixed
-        case flexible
-        case local
-        case removable
-        
-        var id: String {
-            rawValue
-        }
-    }
-}
