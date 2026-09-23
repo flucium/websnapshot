f07681bc4ee9b1e61cb57ee3b0e7a9133c73ffa6
flucium <flucium@flucium.net>
2026-09-23 16:36:45 +0900

    refactor ./WebSnapshot/Shared/Utilities/OCR.swift

--- WebSnapshot/Shared/Utilities/OCR.swift
+++ WebSnapshot/Shared/Utilities/OCR.swift
@@ -3,10 +3,7 @@ import CoreGraphics
 import Vision
 
 final class OCR {
-    nonisolated static func recognizeText(
-        _ image: CGImage,
-        _ recognitionLanguages: [Locale.Language] = []
-    ) async throws -> String {
+    nonisolated static func recognizeText(_ image: CGImage,_ recognitionLanguages: [Locale.Language] = []) async throws -> String {
         
         var request = RecognizeTextRequest()
         
@@ -19,34 +16,20 @@ final class OCR {
         request.usesLanguageCorrection = true
         
         do{
-            let observations = try await request.perform(
-                on: image
-            )
+            let observations = try await request.perform(on: image)
             
             return observations
                 .compactMap {
-                    $0.topCandidates(
-                        1
-                    ).first?.string
+                    $0.topCandidates(1).first?.string
                 }
-                .joined(
-                    separator: "\n"
-                )
+                .joined(separator: "\n")
             
         } catch let error as CancellationError {
             throw error
         } catch let error as AppError {
             throw error
         } catch {
-            throw AppError
-                .textRecognitionFailed(
-                "Text could not be recognized on this page.",
-                error.localizedDescription,
-                error
-            )
+            throw AppError.textRecognitionFailed("Text could not be recognized on this page.",error.localizedDescription,error)
         }
-        
-        
-
     }
 }
