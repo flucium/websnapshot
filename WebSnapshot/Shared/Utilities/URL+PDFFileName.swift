93e64c84666c32a8af0e334fadb43a06834ff927
flucium <flucium@flucium.net>
2026-09-23 16:40:39 +0900

    refactor ./WebSnapshot/Shared/Utilities/URL+PDFFileName.swift

--- WebSnapshot/Shared/Utilities/URL+PDFFileName.swift
+++ WebSnapshot/Shared/Utilities/URL+PDFFileName.swift
@@ -2,32 +2,49 @@ import Foundation
 
 extension URL {
     
-    static func pdfFileName(
-        _ title:String?,
-        _ url: URL?
-    ) -> String {
+    static func pdfFileName(_ title:String?,_ url: URL?,_ suffix: String = "") -> String {
+        
         let trimmedTitle = title?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        
         let invalidCharacters = CharacterSet(charactersIn: "/\\?%*|\"<>:")
+        
         let cleanTitle = trimmedTitle.components(separatedBy: invalidCharacters).joined()
+        
         let source = cleanTitle.isEmpty ? (url?.removingScheme ?? "page") : cleanTitle
-        let name = source.components(
-            separatedBy: invalidCharacters
-        ).joined()
+        
+        let name = source.components(separatedBy: invalidCharacters).joined()
+        
         let baseName = name.lowercased().hasSuffix(".pdf") ? String(name.dropLast(4)) : name
-        return (baseName.isEmpty ? "page" : baseName) + ".pdf"
+        
+        let byteLimit = 240 - suffix.utf8.count - ".pdf".utf8.count
+        
+        var shortenedName = ""
+        
+        var byteCount = 0
+        
+        for character in baseName {
+        
+            let size = String(character).utf8.count
+            
+            guard byteCount + size <= byteLimit else {
+                break
+            }
+            
+            shortenedName.append(character)
+            
+            byteCount += size
+        }
+        
+        return (shortenedName.isEmpty ? "page" : shortenedName) + suffix + ".pdf"
     }
     
     
     private var removingScheme: String {
-        guard let scheme = self.absoluteString.range(
-            of: "://"
-        ) else {
+        guard let scheme = self.absoluteString.range(of: "://") else {
             return self.absoluteString
         }
         
-        return String(
-            self.absoluteString[scheme.upperBound...]
-        )
+        return String(self.absoluteString[scheme.upperBound...])
     }
     
 }
